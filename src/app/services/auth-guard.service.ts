3c10fc3c56f8fc6127bd19f17fa87d7f4237de21
Ismail NORRI <ismail.norri.24@gmail.com>
2020-05-07 01:26:40 +0000

    Authentification complete avec AuthGuard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  canActivate(): boolean | Observable<boolean> | Promise<boolean> {
+    return new Promise(
+      (Resolve, Reject) => {
+        firebase.auth().onAuthStateChanged(
+          (user) => {
+            if (user) {
+              Resolve(true);
+            } else {
+              this.router.navigate(['/auth', 'signin']);
+              Resolve(false);
+            }
+          }
+        );
+      }
+    );
+  }
 }
